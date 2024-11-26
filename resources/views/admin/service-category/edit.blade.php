ebeef3487e57cfafd212c79cf95c1bcedfc08a3e
Your Name <youremail@example.com>
2024-11-26 17:51:13 +0530

    cateory

--- resources/views/admin/service-category/edit.blade.php
+++ resources/views/admin/service-category/edit.blade.php
@@ -57,7 +57,7 @@
                                 </div>
                             </div> --}}
                         </div>
-                        <div class="col-md-6">
+                        {{-- <div class="col-md-6">
                             <div class="col-md-6">
                                 <div class="form-group">
                                     <label>Previous Image</label>
@@ -66,7 +66,7 @@
                                     </div>
                                 </div>
                             </div>
-                        </div>
+                        </div> --}}
                         <div class="row">
                             <div class="col-md-6">
                                 <div class="mb-3">
