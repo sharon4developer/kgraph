f8ab96a4a6e7c28175991e628ad3d65d8113fe7c
Your Name <youremail@example.com>
2024-12-27 23:11:19 +0530

    bug fixing

--- resources/views/frontend/Common/footer.blade.php
+++ resources/views/frontend/Common/footer.blade.php
@@ -1,4 +1,61 @@
 <style>
+/* Custom list styles for Services */
+.services-list {
+    list-style: none; /* Remove default bullets */
+    padding: 0;
+    margin: 0;
+}
+
+.services-list li {
+    position: relative;
+    padding-left: 25px; /* Space for bullet image */
+    margin-bottom: 10px; /* Space between items */
+}
+
+.services-list li::before {
+    content: "";
+    background-image: url(assets/listbullets.png);
+    background-size: contain;
+    background-repeat: no-repeat;
+    width: 16px; /* Adjust based on your image size */
+    height: 16px;
+    position: absolute;
+    left: 0;
+    top: 50%;
+    transform: translateY(-50%);
+}
+
+/* Custom list styles for Useful Links */
+.links-list {
+    list-style: none; /* Remove default bullets */
+    padding: 0;
+    margin: 0;
+}
+
+.links-list li {
+    position: relative;
+    padding-left: 25px; /* Space for bullet image */
+    margin-bottom: 10px; /* Space between items */
+}
+
+.links-list li::before {
+    content: "";
+    background-image: url(assets/nowlistbullets.png); /* Replace with the actual path to your arrow icon */
+    background-size: contain;
+    background-repeat: no-repeat;
+    width: 16px; /* Adjust based on your image size */
+    height: 16px;
+    position: absolute;
+    left: 0;
+    top: 50%;
+    transform: translateY(-50%);
+}
+
+/* Hover effect for links */
+.services-list li a:hover,
+.links-list li a:hover {
+    text-decoration: underline;
+}
 
 </style>
 
@@ -9,40 +66,44 @@ $serviceCategories = ServiceCategory::select('image','id','title','alt_tag','slu
 
 <footer class="w-full footer-secpernt h-full relative overflow-hidden">
     <div class="globebg">
-        <div class="relative w-full footer z-30 py-6 md:pt-10 md:pb-6 bg-[#062357B2]">
-            <div class="container mx-auto px-5 lg:px-12 h-full w-full py-4  lg:py-0 flex flex-col lg:flex-row justify-between gap-10 lg:gap-52 2xl:gap-72 items-start">
-                <div class="w-full lg:w-1/2 2xl:w-[65%] flex flex-col gap-8 lg:gap-16">
+        <div class="relative w-full footer z-30 py-6 md:pt-28 md:pb-6">
+            <div class="container mx-auto px-5 lg:px-12 h-full w-full py-4  lg:py-0 flex flex-col lg:flex-row justify-between gap-40  items-start">
+                <div class="w-full flex flex-col gap-6">
                     <div class="flex items-center gap-5">
                         <img class="" src="{{ asset('assets/home_Banner/grouplogo.png') }}" alt="K-graph logo">
                     </div>
 
-                    <div class="">
-                        <ul class="font_inter flex flex-col lg:flex-row lg:items-center gap-6">
-                            <li class=""><a class="arrow-comig font-light font_roboto text-white text-lg lg:text-xs 2xl:text-lg" href="{{ url('about-us') }}">About</a></li>
-                            <li class=""><a class="arrow-comig font-light font_roboto text-white text-lg lg:text-xs 2xl:text-lg" href="{{ url('careers') }}">Careers</a></li>
-                            <li class=""><a class="arrow-comig font-light font_roboto text-white text-lg lg:text-xs 2xl:text-lg" href="{{ url('packages') }}">Packages</a></li>
-                            <li class=""><a class="arrow-comig font-light font_roboto text-white text-lg lg:text-xs 2xl:text-lg" href="{{ url('contact-us') }}">Contact us</a></li>
-                        </ul>
-                    </div>
-
-                    <div class="flex flex-col 2xl:flex-row 2xl:items-center gap-5">
-                        <div class="flex items-center gap-3 mr-[5px]">
-                            <img class="w-[60px]" src="{{ asset('assets/bluenooneedvisa.png') }}" alt="visa no need">
-                            <div class="text-lg font_jakartafoot font-semibold font_roboto text-white">Need any support fortour and visa?</div>
-                        </div>
-
-                        <div class="flex items-center gap-3">
-                            <img class="w-[60px]" src="{{ asset('assets/bluetravelstarted.png') }}" alt="started travelling">
-                            <div class="text-lg font_jakartafoot font-semibold font_roboto text-white">Are you ready forget started travelling?</div>
-                        </div>
-                    </div>
+                    <p class="font_inter text-[12px]  font-light lg:text-justify capitalize  text-white">simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
                 </div>
 
-                <div class="w-full lg:w-1/2 2xl:w-[35%]">
-                    <h6 class="font_inter text-[30px] lg:text-4xl font-semibold text-white">GET THE LATEST NEWS AND INSIGHTS</h6>
-                    <p class="font_inter text-[14px] lg:text-base font-semibold lg:text-justify pt-8 capitalize pb-12 text-white">simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
-
-                    <div class="flex items-center gap-3 pt-10 md:pt-0">
+                <div class="flex gap-40">
+                    <!-- Services Section -->
+                    <div>
+                        <h4 class="text-lg font-bold mb-4 text-white">Services</h4>
+                        <ul class="services-list">
+                            <li><a href="#" class="text-sm hover:underline text-white whitespace-nowrap">Mistakes To Avoid</a></li>
+                            <li><a href="#" class="text-sm hover:underline text-white whitespace-nowrap">Your Startup</a></li>
+                            <li><a href="#" class="text-sm hover:underline text-white whitespace-nowrap">Know About Fonts</a></li>
+                            <li><a href="#" class="text-sm hover:underline text-white whitespace-nowrap">Case Studies</a></li>
+                        </ul>
+                    </div>
+                
+                    <!-- Useful Links Section -->
+                    <div>
+                        <h4 class="text-lg font-bold mb-4 text-white">Useful Links</h4>
+                        <ul class="links-list">
+                            <li><a href="#" class="text-sm hover:underline text-white whitespace-nowrap">Latest News</a></li>
+                            <li><a href="#" class="text-sm hover:underline text-white whitespace-nowrap">Careers</a></li>
+                            <li><a href="#" class="text-sm hover:underline text-white whitespace-nowrap">General Inquiries</a></li>
+                            <li><a href="#" class="text-sm hover:underline text-white whitespace-nowrap">Case Studies</a></li>
+                        </ul>
+                    </div>
+                </div>
+                
+                
+                <div class="w-full">
+                    <h6 class="font_inter text-[30px] lg:text-[33px] font-semibold text-white">GET THE LATEST NEWS AND INSIGHTS</h6>
+                    <div class="flex items-center gap-3 pt-12">
                         <span class="capitalize font-bold text-sm text-white font_roboto hidden lg:block">Subscribe</span>
                         <form id="news-letter-add-form" class="relative">
                             <div class="bg-white rounded-[50px] flex items-center overflow-hidden glow-effect">
@@ -52,20 +113,20 @@ $serviceCategories = ServiceCategory::select('image','id','title','alt_tag','slu
                         </form>
                     </div>
 
-                    <div class="flex gap-5 flex-col lg:items-center pt-10">
+                    <div class="flex gap-5 flex-col pt-10">
                         <div class="capitalize font-bold text-sm text-white font_inter">Subscribe</div>
                         <div class=" flex items-center gap-5">
                             <a href="https://www.facebook.com/KGraphimmigration/" target="_blank" rel="noopener noreferrer">
-                                <img  class="w-auto h-[28px] transition-transform duration-300 transform hover:scale-125" src="{{ asset('assets/fbvector.png') }}" alt="facebook">
+                                <img  class="w-auto h-[20px] transition-transform duration-300 transform hover:scale-125" src="{{ asset('assets/fbvector.png') }}" alt="facebook">
                             </a>
                             <a href="https://www.instagram.com/kgraph_immigration?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" target="_blank" rel="noopener noreferrer">
-                                <img  class="w-auto h-[28px] transition-transform duration-300 transform hover:scale-125" src="{{ asset('assets/instagaramvector.png') }}" alt="instagram">
+                                <img  class="w-auto h-[20px] transition-transform duration-300 transform hover:scale-125" src="{{ asset('assets/instagaramvector.png') }}" alt="instagram">
                             </a>
                             <a href="https://ca.linkedin.com/company/kgraph-homeabout-usstudy-abroadcanada-immigrationieltsother-servicescontact-1-416-989-7788-91-94476" target="_blank" rel="noopener noreferrer">
-                                <img  class="w-auto h-[28px] transition-transform duration-300 transform hover:scale-125" src="{{ asset('assets/linkedinvector.png') }}" alt="linkedin">
+                                <img  class="w-auto h-[20px] transition-transform duration-300 transform hover:scale-125" src="{{ asset('assets/linkedinvector.png') }}" alt="linkedin">
                             </a>
                             <a href="https://www.youtube.com/@kgraphimmigration8686/featured" target="_blank" rel="noopener noreferrer">
-                                <img  class="w-auto h-[28px] transition-transform duration-300 transform hover:scale-125" src="{{ asset('assets/home_Banner/youtubenew.png') }}" alt="youtube">
+                                <img  class="w-auto h-[20px] transition-transform duration-300 transform hover:scale-125" src="{{ asset('assets/home_Banner/youtubenew.png') }}" alt="youtube">
                             </a>
                         </div>
                     </div>
@@ -74,14 +135,14 @@ $serviceCategories = ServiceCategory::select('image','id','title','alt_tag','slu
 
         </div>
     </div>
-    <div class="bg-black">
+    <div class="bg-white">
         <div class="container mx-auto px-5 lg:px-12 h-full w-full py-4  lg:py-0 lg:flex justify-between gap-72 items-start">
             <div class="py-4 flex flex-col md:flex-row md:items-center gap-6">
-                <div class="text-white font_jakartafoot font-normal text-xs opacity-80">© 2024 - K-graph Canadian Immigration Services.</div>
+                <div class="text-black font_jakartafoot font-normal text-xs opacity-80">© 2024 - K-graph Canadian Immigration Services.</div>
                 <div class="flex items-center gap-7">
-                    <div><a class="text-white font_jakartafoot font-normal text-xs" href="{{ url('terms-and-conditions') }}">Terms and conditions</a></div>
-                    <div><a class="text-white font_jakartafoot font-normal text-xs" href="{{ url('privacy-policy') }}">Privacy Policy</a></div>
-                    <div><a class="text-white font_jakartafoot font-normal text-xs" href="{{ url('contact-us') }}">Contact Us</a></div>
+                    <div><a class="text-black font_jakartafoot font-normal text-xs" href="{{ url('terms-and-conditions') }}">Terms and conditions</a></div>
+                    <div><a class="text-black font_jakartafoot font-normal text-xs" href="{{ url('privacy-policy') }}">Privacy Policy</a></div>
+                    <div><a class="text-black font_jakartafoot font-normal text-xs" href="{{ url('contact-us') }}">Contact Us</a></div>
                 </div>
             </div>
         </div>
